4c84e39faf2d6f4060e7715c6672e28fc4ba122b
darasdarmawan <darasyauqidarmawan@gmail.com>
2024-03-15 11:13:08 +0700

    membuat display

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -41,6 +41,18 @@ void insertionSort() {
     
 }
 
+void display() {
+	cout << endl;
+	cout << "===================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "===================" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl;
+	}
+	cout << "Jumlah pass = " << n - 1 << endl;
+	cout << endl;
+
+
 
 
 
